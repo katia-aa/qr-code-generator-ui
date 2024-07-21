881abc344e832bcb107b6e2c6b4dc4b1db706314
Katia <11549223+katia-aa@users.noreply.github.com>
2024-07-21 17:12:02 -0400

    Prepare app for deployment
    - Modify tsconfig
    - Add base to vite config
    - Format App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Container,
   Box,
@@ -6,20 +6,23 @@ import {
   Input,
   Button,
   Image,
-} from '@chakra-ui/react';
-import { useMutation, UseMutationResult } from '@tanstack/react-query';
+} from "@chakra-ui/react";
+import { useMutation, UseMutationResult } from "@tanstack/react-query";
 
 const fetchQRCode = async (text: string): Promise<string> => {
-  const response = await fetch('https://qr-code-generator-server.herokuapp.com/generate', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ text }),
-  });
+  const response = await fetch(
+    "https://qr-code-generator-p8ou.onrender.com/generate",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text }),
+    }
+  );
 
   if (!response.ok) {
-    throw new Error('Network response was not ok');
+    throw new Error("Network response was not ok");
   }
 
   const blob = await response.blob();
@@ -27,8 +30,12 @@ const fetchQRCode = async (text: string): Promise<string> => {
 };
 
 const App: React.FC = () => {
-  const [text, setText] = useState<string>('');
-  const mutation: UseMutationResult<string, Error, string> = useMutation<string, Error, string>({
+  const [text, setText] = useState<string>("");
+  const mutation: UseMutationResult<string, Error, string> = useMutation<
+    string,
+    Error,
+    string
+  >({
     mutationFn: fetchQRCode,
   });
 
@@ -36,10 +43,9 @@ const App: React.FC = () => {
     mutation.mutate(text);
   };
 
-
   return (
     <Container centerContent>
-      <Box p={4} display={{ md: 'flex' }}>
+      <Box p={4} display={{ md: "flex" }}>
         <Box flexShrink={0}>
           <Heading as="h1" size="xl" my={4}>
             QR Code Generator
@@ -50,7 +56,11 @@ const App: React.FC = () => {
             onChange={(e) => setText(e.target.value)}
             mb={4}
           />
-          <Button colorScheme="teal" onClick={handleGenerate} isLoading={mutation.isPending}>
+          <Button
+            colorScheme="teal"
+            onClick={handleGenerate}
+            isLoading={mutation.isPending}
+          >
             Generate QR Code
           </Button>
           {mutation.isSuccess && (
